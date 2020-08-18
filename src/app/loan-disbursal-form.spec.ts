d4b47b26511579b9699fcc663a15890fe1767c8e
sumanthh <sumanthdande@gmail.com>
2020-08-18 23:49:34 +0530

    LoanDisbursal Added

---
+++ src/app/loan-disbursal-form.spec.ts
@@ -0,0 +1,7 @@
+import { LoanDisbursalForm } from './loan-disbursal-form';
+
+describe('LoanDisbursalForm', () => {
+  it('should create an instance', () => {
+    expect(new LoanDisbursalForm()).toBeTruthy();
+  });
+});
--- src/app/loan-disbursal/loan-disbursal.component.ts
+++ src/app/loan-disbursal/loan-disbursal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoanService } from '../loan.service';
+import { LoanRequest } from '../loan-request';
+import { LoanDisbursalForm } from '../loan-disbursal-form';
 
 @Component({
   selector: 'app-loan-disbursal',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoanDisbursalComponent implements OnInit {
 
-  constructor() { }
+  loanRequest:LoanRequest = new LoanRequest();
+  loanRequests:any=[];
+  loanRequestByStatus:any=[];
+  loanDisburseMessage:string;
+  error:string;
+  pendingFlag:boolean=false;
+  requestedFlag:boolean=false;
+  constructor(private loanService:LoanService) { }
 
   ngOnInit() {
+    this.loanService.getPendingLoanRequest().subscribe(data=>{this.loanRequests=data;this.pendingFlag=true}, error=>{this.error=error.error.message;console.log(error)});
   }
 
+  public disburseLoanRequests(request:LoanRequest,option:string){
+    let loanDisbursalForm:LoanDisbursalForm = new LoanDisbursalForm();
+    loanDisbursalForm.loanRequestId = request.loanRequestId;
+    loanDisbursalForm.option = option;
+    this.loanService.loanDisburse(loanDisbursalForm).subscribe(data=>{this.loanDisburseMessage=data.message});
+    this.loanRequests = this.loanRequests.filter(item=>item != request);
+  }
+
+  public getLoanRequestByStatus(status:string){
+    return this.loanService.getLoanRequestByStatus(status).subscribe(
+      data=>{this.loanRequestByStatus=data;
+             this.requestedFlag=true;})
+  }
+
+  public pendingLoanrequest(){
+    this.loanService.getPendingLoanRequest().subscribe(data=>{this.loanRequests=data;this.pendingFlag=true;this.requestedFlag=false;}, error=>{this.error=error.error.message;console.log(error)});
+  }
 }
